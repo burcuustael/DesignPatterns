5f9e2cbfc97862480076dbcc03fad3c4a9452df7
burcuustael <burcustael@gmail.com>
2023-08-01 17:00:38 +0300

    Chain Of Responsibilty Design Pattern ı tamamlandı.

---
+++ ChainOfResponsibilty/DesignPattern.ChainOfResponsibilty/ChainOfResponsibilty/AreaDirector.cs
@@ -0,0 +1,34 @@
+﻿using DesignPattern.ChainOfResponsibilty.DAL;
+using DesignPattern.ChainOfResponsibilty.Models;
+
+namespace DesignPattern.ChainOfResponsibilty.ChainOfResponsibilty
+{
+    public class AreaDirector : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+
+            if (req.Amount <= 400000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Direktörü - Zeynep Yılmaz";
+                customerProcess.Description = "Para Çekme İşlemi Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Direktörü - Zeynep Yılmaz";
+                customerProcess.Description = "Para Çekme Tutarı Bölge Direktörünün Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Gerçekleştirilemedi, Müşterinin Günlük Maksimum Çekebileceği Tutar 400.000 ₺";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- ChainOfResponsibilty/DesignPattern.ChainOfResponsibilty/DAL/Context.cs
+++ ChainOfResponsibilty/DesignPattern.ChainOfResponsibilty/DAL/Context.cs
@@ -1,6 +1,16 @@
-﻿namespace DesignPattern.ChainOfResponsibilty.DAL
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.ChainOfResponsibilty.DAL
 {
-    public class Context
+    public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-KOJI8U6;initial catalog=DesignPattern1; integrated security=true;");
+
+            base.OnConfiguring(optionsBuilder); 
+        }
+
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
     }
 }
